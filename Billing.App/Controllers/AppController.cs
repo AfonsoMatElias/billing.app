a0023c14b77bd77001dfe2a9748bc7b2c3a10575
Afonso Matumona <afonsomatumona@hotmail.com>
2022-06-23 11:55:36 +0100

    feat: added user preferences handling

--- Billing.App/Controllers/AppController.cs
+++ Billing.App/Controllers/AppController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,139 +15,180 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Billing.App.Controllers
 {
-    public class AppController : Controller
-    {
-        private IAuth mAuth;
-        private FileHandler fileHandler;
+	public class AppController : Controller
+	{
+		private IAuth mAuth;
+		private FileHandler fileHandler;
+		private PreferenceHandler preferenceHandler;
 
-        public AppController(IAuth mAuth, FileHandler fileHandler)
-        {
-            this.mAuth = mAuth;
-            this.fileHandler = fileHandler;
-        }
+		public AppController(IAuth mAuth, FileHandler fileHandler, PreferenceHandler preferenceHandler)
+		{
+			this.mAuth = mAuth;
+			this.fileHandler = fileHandler;
+			this.preferenceHandler = preferenceHandler;
+		}
 
-        public IActionResult Index()
-            => Redirect("/index.html");
+		public IActionResult Index()
+			=> Redirect("/index.html");
 
-        [HttpPost(WebRoutes.SignIn)]
-        public async Task<IActionResult> SignIn([FromBody] SignInModel model)
-        {
-            if (model == null)
-                return BadRequest("O Modelo não pode ser nulo.");
+		[HttpPost(WebRoutes.SignIn)]
+		public async Task<IActionResult> SignIn([FromBody] SignInModel model)
+		{
+			if (model == null)
+				return BadRequest("O Modelo não pode ser nulo.");
 
-            // Signing in
-            return Ok(await mAuth.SignIn(new SignInModel
-            {
-                User = model.User,
-                Password = model.Password
-            }));
-        }
+			// Signing in
+			return Ok(await mAuth.SignIn(new SignInModel
+			{
+				User = model.User,
+				Password = model.Password
+			}));
+		}
 
-        [HttpPost(WebRoutes.SignUp)]
-        public async Task<IActionResult> SignUp([FromBody] SignUpModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                var errors = new Dictionary<string, string>();
-                foreach (var key in ModelState.Keys)
+		[HttpPost(WebRoutes.SignUp)]
+		public async Task<IActionResult> SignUp([FromBody] SignUpModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				var errors = new Dictionary<string, string>();
+				foreach (var key in ModelState.Keys)
+				{
+					var value = ModelState.GetValueOrDefault(key);
+					foreach (var error in value.Errors)
+						errors.Add(key, error.ErrorMessage);
+				}
+
+				return BadRequest(new AuthResponse
+				{
+					Errors = errors.Select(s => $"key: {s.Key}, error: {s.Value}").ToList()
+				});
+			}
+
+			// Sigining up
+			return Ok(await mAuth.SignUp(model, model.Password));
+		}
+
+		[Authorize]
+		[HttpGet(WebRoutes.Account)]
+		public async Task<IActionResult> Account()
+		{
+			var id = HttpContext.GetUserId();
+			if (id == null)
+				return Ok(new Response
+				{
+					Errors = new List<string> { "Deve estar Logado para realizar esta operação" }
+				});
+
+			var result = await mAuth.Account(id);
+			if (!result.Success)
+				return Ok(new Response
+				{
+					Errors = result.Errors.ToList()
+				});
+
+            var userName = result.Data.UserName;
+
+			try
+			{
+                if (!this.preferenceHandler.HasPreferences(userName)) 
                 {
-                    var value = ModelState.GetValueOrDefault(key);
-                    foreach (var error in value.Errors)
-                        errors.Add(key, error.ErrorMessage);
+                    // Creating the preferencies
+                    this.preferenceHandler.InitPreferences(userName);
+                    await this.preferenceHandler.Save();
                 }
 
-                return BadRequest(new AuthResponse
-                {
-                    Errors = errors.Select(s => $"key: {s.Key}, error: {s.Value}").ToList()
-                });
-            }
+                result.Data.Preferences = await this.preferenceHandler.Get(userName);
+			}
+			catch (AppException)
+			{
+				Debugger.Log(0, "Preferences Exception", $"Couldn't create preferences for {userName}");
+			}
 
-            // Sigining up
-            return Ok(await mAuth.SignUp(model, model.Password));
-        }
+			// Get Logged user info
+			return Ok(result);
+		}
 
-        [Authorize]
-        [HttpGet(WebRoutes.Account)]
-        public async Task<IActionResult> Account()
-        {
-            var id = HttpContext.GetUserId();
-            if (id == null)
-                return Ok(new Response
-                {
-                    Errors = new List<string> { "Deve estar Logado para realizar esta operação" }
-                });
-
-            var result = await mAuth.Account(id);
-            if (!result.Success)
-                return Ok(new Response
-                {
-                    Errors = result.Errors.ToList()
-                });
-
-            // Get Logged user info
-            return Ok(result);
-        }
-
-        [Authorize]
-        [HttpPut(WebRoutes.ChangeRoles)]
-        public async Task<IActionResult> ChangeRoles(string id, [FromBody] string[] roles)
-        {
-            return Ok(await mAuth.ChangeRoles(id, roles));
-        }
-
-        [Authorize]
-        [HttpPut(WebRoutes.ChangePassword)]
-        public async Task<IActionResult> ChangePassword([FromBody] PasswordModel model)
-        {
-            return Ok(await mAuth.ChangePassword(HttpContext.GetUserId(), model.Current, model.New));
-        }
-
-        [Authorize]
-        [HttpPut(WebRoutes.ChangeUserInfo)]
-        public async Task<IActionResult> ChangeUserInfo(string id, [FromBody] SignUpModel model)
-        {
-            return Ok(await mAuth.ChangeUserInfo(id, model));
-        }
-
-        [HttpGet(WebRoutes.Roles)]
-        public async Task<IActionResult> GetRoles()
-        {
-            return Ok(await Task.FromResult(new Response
-            {
-                Data = typeof(AppRoles).GetFields()
-                    .Select(role => role.GetValue(role) as string)
-                    .Where(x => x != AppRoles.superadmin)
-                    .ToList()
-            }));
-        }
-
-        [Authorize]
-        [HttpGet(WebRoutes.UserRoles)]
-        public async Task<IActionResult> GetUserRoles(string userId)
-        {
-            return Ok(await mAuth.UserRoles(userId));
-        }
-
-        // [Authorize]
-        [HttpGet(WebRoutes.Download)]
-        public async Task<IActionResult> Download(string folderName, string fileName)
-        {
+		[Authorize]
+		[HttpPut(WebRoutes.ChangePreference)]
+		public async Task<IActionResult> ChangeRoles(string prefName, [FromQuery] string prefValue)
+		{
+            var userName = this.HttpContext.GetKey("name");
             
-            var file = Path.Combine(fileHandler.Folder(folderName).folderPath,fileName);   
+            if (prefValue == null)
+                return Ok(new Response {
+                    Errors = new[] { "Invalid value provided" }
+                });
 
-            // IF file does no exist
-            if (!System.IO.File.Exists(file))
-                return NotFound();
-            
-            // Get all the data as bytes
-            var dataBytes = await System.IO.File.ReadAllBytesAsync(file);
+            await this.preferenceHandler.Set(userName, prefName, prefValue);
 
-            // If the bytes is empty 
-            if (dataBytes.Count() == 0)
-                return NoContent();
+            await this.preferenceHandler.Save();
 
-            // Stream out the data
-            return File(dataBytes, MimeTypeMapping.Get(fileName.ToExtension()));
-        }
-    }
+			return Ok(new Response {
+                Data = true,
+                Message = "Updated"
+            });
+		}
+
+		[Authorize]
+		[HttpPut(WebRoutes.ChangeRoles)]
+		public async Task<IActionResult> ChangeRoles(string id, [FromBody] string[] roles)
+		{
+			return Ok(await mAuth.ChangeRoles(id, roles));
+		}
+
+		[Authorize]
+		[HttpPut(WebRoutes.ChangePassword)]
+		public async Task<IActionResult> ChangePassword([FromBody] PasswordModel model)
+		{
+			return Ok(await mAuth.ChangePassword(HttpContext.GetUserId(), model.Current, model.New));
+		}
+
+		[Authorize]
+		[HttpPut(WebRoutes.ChangeUserInfo)]
+		public async Task<IActionResult> ChangeUserInfo(string id, [FromBody] SignUpModel model)
+		{
+			return Ok(await mAuth.ChangeUserInfo(id, model));
+		}
+
+		[HttpGet(WebRoutes.Roles)]
+		public async Task<IActionResult> GetRoles()
+		{
+			return Ok(await Task.FromResult(new Response
+			{
+				Data = typeof(AppRoles).GetFields()
+					.Select(role => role.GetValue(role) as string)
+					.Where(x => x != AppRoles.superadmin)
+					.ToList()
+			}));
+		}
+
+		[Authorize]
+		[HttpGet(WebRoutes.UserRoles)]
+		public async Task<IActionResult> GetUserRoles(string userId)
+		{
+			return Ok(await mAuth.UserRoles(userId));
+		}
+
+		// [Authorize]
+		[HttpGet(WebRoutes.Download)]
+		public async Task<IActionResult> Download(string folderName, string fileName)
+		{
+
+			var file = Path.Combine(fileHandler.Folder(folderName).folderPath, fileName);
+
+			// IF file does no exist
+			if (!System.IO.File.Exists(file))
+				return NotFound();
+
+			// Get all the data as bytes
+			var dataBytes = await System.IO.File.ReadAllBytesAsync(file);
+
+			// If the bytes is empty 
+			if (dataBytes.Count() == 0)
+				return NoContent();
+
+			// Stream out the data
+			return File(dataBytes, MimeTypeMapping.Get(fileName.ToExtension()));
+		}
+	}
 }
---
+++ Billing.App/Handlers/PreferenceHandler.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Billing.Shared.Helpers;
+using Newtonsoft.Json;
+
+namespace Billing.App.Handlers
+{
+	public class PreferenceHandler
+	{
+
+		private Dictionary<string, Dictionary<string, object>> data = new Dictionary<string, Dictionary<string, object>>();
+		private string path;
+
+		public PreferenceHandler()
+		{
+			this.path = IoC.Configuration["Preferences"];
+
+			// Create file if it does not exists
+			if (!File.Exists(path)) File.WriteAllText(path, "{}");
+
+			// Reading all the data
+			var content = File.ReadAllText(path);
+
+			// Converting to the object
+			this.data = JsonConvert
+				.DeserializeObject<Dictionary<string, Dictionary<string, object>>>(content);
+		}
+
+		public bool HasPreferences(string user)
+		{
+			return this.GetUserPreferences(user) != null;
+		}
+
+		public Dictionary<string, object> InitPreferences(string user)
+		{
+			// Creating new preferences for the user
+			Dictionary<string, object> pref = new Dictionary<string, object>
+			{
+				{"messagePopup", true},
+				{"notificationPopup", true},
+				{"cashBoxOpenCloseConfirmation", false},
+				{"darkMode", false},
+			} as Dictionary<string, object>;
+
+			// Adding to the object
+			this.data.TryAdd(user, pref);
+
+			return pref;
+		}
+
+		private Dictionary<string, object> GetUserPreferences(string user)
+		{
+			// Getting the value from the storage
+			return this.data.GetValueOrDefault(user);
+		}
+
+		public async Task<Dictionary<string, object>> Get(string user)
+		{
+			// Getting user preferences
+			var pref = this.GetUserPreferences(user);
+
+			// Resolving the value 
+			return await Task.FromResult(pref);
+		}
+
+		public async Task Set(string user, string name, string value)
+		{
+			// Getting user preferences
+			var pref = this.GetUserPreferences(user);
+
+			// Checking if exists
+			if (pref == null)
+			{
+				// Creating new preferences for the user
+				pref = this.InitPreferences(user);
+			}
+
+			// Setting the wanted value
+			pref[name] = Convert.ChangeType(value, pref[name].GetType());
+
+			await Task.FromResult(0);
+		}
+
+		public async Task Save()
+		{
+			// Saving all the changes
+			await File.WriteAllTextAsync(this.path, JsonConvert.SerializeObject(this.data));
+		}
+	}
+}
