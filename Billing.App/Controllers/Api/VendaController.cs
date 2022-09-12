dd8cac0f833cc9832392a540b48ead87fa6a7600
Afonso Matumona <afonsomatumona@hotmail.com>
2022-09-12 09:44:04 +0100

    feat: added vendaItens in venda get request

--- Billing.App/Controllers/Api/VendaController.cs
+++ Billing.App/Controllers/Api/VendaController.cs
@@ -7,81 +7,94 @@ using Billing.Service.Services.Interfaces;
 using Billing.Service.Responses;
 using Billing.Service.Pageable;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace Billing.App.Controllers.Api
 {
-    [Route(ApiRoutes.Base)]
-    [ApiController]
-    // [Authorize]
-    public class VendaController : ControllerBase
-    {
-        private IVendaService service = null;
+	[Route(ApiRoutes.Base)]
+	[ApiController]
+	// [Authorize]
+	public class VendaController : ControllerBase
+	{
+		private IVendaService service = null;
 
-        public VendaController(IVendaService service)
-        {
-            this.service = service;
-        }
+		public VendaController(IVendaService service)
+		{
+			this.service = service;
+		}
 
-        // GET: api/Venda
-        [HttpGet]
-        public async Task<Response> Get([FromQuery] PageableQueryParam pageableQuery)
-        {
-            var dbData = await service.FindAll(Pagination.Of(pageableQuery.Page, pageableQuery.Size), 
-            queryable => {
-                return queryable.Include(x => x.Cliente.Pessoa)
-                                .Include(x => x.TipoVenda);
-            });
+		// GET: api/Venda
+		[HttpGet]
+		public async Task<Response> Get([FromQuery] PageableQueryParam pageableQuery, bool ignoreVendaItens = true)
+		{
+			var dbData = await service.FindAll(Pagination.Of(pageableQuery.Page, pageableQuery.Size),
+			queryable =>
+			{
+				return queryable.Include(x => x.Cliente.Pessoa)
+								.Include(x => x.VendaItens)
+								.Include(x => x.TipoVenda);
+			});
 
-            return new Response
-            {
-                Data = dbData.Data,
-                Pagination = dbData.Pageable,
-                Message = "Listed"
-            };
-        }
+            var @return = dbData.Data;
 
-        // GET: api/Venda/5:12837918237
-        [HttpGet("{uid}")]
-        public async Task<Response> Get(string uid)
-        {
-            var dbData = await service.FindById(uid);
+            if (ignoreVendaItens)
+                @return = dbData.Data.Select(x =>
+				{
+					var venda = x.IgnoreProperties(new string[] { nameof(VendaDto.VendaItens) });
+					venda.TotalVendaItens = x.VendaItens.Count();
+					return venda;
+				}).ToList();
 
-            return new Response
-            {
-                Data = dbData,
-                Message = "Response Object",
-                Errors = dbData == null ? new[] { "Not Found" } : new string[] { }
-            };
-        }
+			return new Response
+			{
+				Data = @return,
+				Pagination = dbData.Pageable,
+				Message = "Listed"
+			};
+		}
 
-        // POST: api/Venda
-        [HttpPost]
-        public async Task<Response> Post([FromBody] VendaDto model)
-        {
-            if (model == null)
-                throw new AppException("Objecto inválido!", true);
+		// GET: api/Venda/5:12837918237
+		[HttpGet("{uid}")]
+		public async Task<Response> Get(string uid)
+		{
+			var dbData = await service.FindById(uid);
 
-            await service.Save(model);
-            return new Response { Message = "Created" };
-        }
+			return new Response
+			{
+				Data = dbData,
+				Message = "Response Object",
+				Errors = dbData == null ? new[] { "Not Found" } : new string[] { }
+			};
+		}
 
-        // PUT: api/Venda/5:12837918237
-        [HttpPut("{uid}")]
-        public async Task<Response> Put(string uid, [FromBody] VendaDto model)
-        {
-            if (model == null)
-                throw new AppException("Objecto inválido!", true);
+		// POST: api/Venda
+		[HttpPost]
+		public async Task<Response> Post([FromBody] VendaDto model)
+		{
+			if (model == null)
+				throw new AppException("Objecto inválido!", true);
 
-            await service.Update(uid, model);
-            return new Response { Message = "Updated" };
-        }
+			await service.Save(model);
+			return new Response { Message = "Created" };
+		}
 
-        // DELETE: api/Venda/5:12837918237
-        [HttpDelete("{uid}")]
-        public async Task<Response> Delete(string uid)
-        {
-            await service.Remove(uid);
-            return new Response { Message = "Deleted" };
-        }
-    }
+		// PUT: api/Venda/5:12837918237
+		[HttpPut("{uid}")]
+		public async Task<Response> Put(string uid, [FromBody] VendaDto model)
+		{
+			if (model == null)
+				throw new AppException("Objecto inválido!", true);
+
+			await service.Update(uid, model);
+			return new Response { Message = "Updated" };
+		}
+
+		// DELETE: api/Venda/5:12837918237
+		[HttpDelete("{uid}")]
+		public async Task<Response> Delete(string uid)
+		{
+			await service.Remove(uid);
+			return new Response { Message = "Deleted" };
+		}
+	}
 }
