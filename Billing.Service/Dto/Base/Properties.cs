12cd30481de511027f54efa88d6da6ab4cba9cbc
AfonsoMatElias <afonsomatumona@hotmail.com>
2023-01-09 09:07:27 +0100

    Switched to dotnet 3.1

--- Billing.Service/Dto/Base/Properties.cs
+++ Billing.Service/Dto/Base/Properties.cs
@@ -4,8 +4,8 @@ namespace Billing.Service.Dto.Base
 {
     public class Properties
     {
-        public long Id { get; init; }
-        public DateTime CreatedAt { get; init; }
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public bool? Visibility { get; set; } = true;
     }
--- Billing.Service/Models/Base/Properties.cs
+++ Billing.Service/Models/Base/Properties.cs
@@ -2,11 +2,11 @@ using System;
 
 namespace Billing.Service.Models.Base
 {
-    public class Properties
-    {
-        public long Id { get; init; }
-        public DateTime CreatedAt { get; init; }
-        public DateTime? UpdatedAt { get; set; }
-        public bool? Visibility { get; set; } = true;
-    }
+	public class Properties
+	{
+		public long Id { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime? UpdatedAt { get; set; }
+		public bool? Visibility { get; set; } = true;
+	}
 }
